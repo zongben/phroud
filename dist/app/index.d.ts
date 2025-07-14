cfaab6f2223df965082d6bd5978eaa77a308d190
ben <ben76020567@gmail.com>
2025-07-14 18:27:15 +0800

    mod: export app

--- dist/app/index.d.ts
+++ dist/app/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./public";
 export * from "./public/interfaces";
 export * from "./public/types";
+export { NextFunction, Request, Response } from "express";
 //# sourceMappingURL=index.d.ts.map
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,4 @@
 export * from "./public";
 export * from "./public/interfaces";
 export * from "./public/types";
+export { NextFunction, Request, Response } from "express";

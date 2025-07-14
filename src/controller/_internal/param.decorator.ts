fd82595caea41d93d828906ec96425495b72206d
ben <ben76020567@gmail.com>
2025-07-14 11:57:16 +0800

    refac: controller internal

---
+++ src/controller/_internal/param.decorator.ts
@@ -0,0 +1,31 @@
+export const PARAM_METADATA_KEY = Symbol("param_metadata");
+type ParamSource = "body" | "query" | "param" | "locals" | "req" | "res";
+
+export type ParamMetadata = {
+  index: number;
+  source: ParamSource;
+  name?: string;
+};
+
+export function createParamDecorator(source: ParamSource) {
+  return function (name?: string): ParameterDecorator {
+    return (target, propertyKey, parameterIndex) => {
+      const existingParams: ParamMetadata[] =
+        Reflect.getMetadata(PARAM_METADATA_KEY, target, propertyKey as any) ||
+        [];
+
+      existingParams.push({
+        index: parameterIndex,
+        source,
+        name,
+      });
+
+      Reflect.defineMetadata(
+        PARAM_METADATA_KEY,
+        existingParams,
+        target,
+        propertyKey as any,
+      );
+    };
+  };
+}

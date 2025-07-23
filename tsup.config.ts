652d456e71c4e6dc177cea7f6cd300977297ff96
ben <ben76020567@gmail.com>
2025-07-23 09:53:17 +0800

    add: tsup

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  outDir: "dist",
+  splitting: false,
+  bundle: true,
+  sourcemap: false,
+  clean: true,
+  target: "es2020",
+});
